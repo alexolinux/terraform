665b76a68d2dcbd7cac7156e65b3bf2c5ff996c6
alexmbarbosa <alexmendes.unix@gmail.com>
2024-04-01 19:09:17 +0100

    Replace provider.tf to eks

--- AWS/study/my-tests/eks/provider.tf
+++ AWS/study/my-tests/eks/provider.tf
@@ -1,1 +1,14 @@
-../provider.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.43.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
