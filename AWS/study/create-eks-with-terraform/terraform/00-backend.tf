1bdfa72826200f2ca2960d973b1b2415bc66329f
alexolinux <alexmendes.unix@gmail.com>
2025-01-21 21:14:58 +0000

    [UPDATE] Change the bucket name in backend.tf

--- AWS/study/create-eks-with-terraform/terraform/00-backend.tf
+++ AWS/study/create-eks-with-terraform/terraform/00-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ax-aws-create-eks-with-terraform-2024"
+    bucket = "ax-aws-create-eks-with-terraform-2099"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
